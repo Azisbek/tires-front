9cf7e4a3ddaa4767bd4d3bced997ed39c4eb17fe
DilSan <norms777666@gmail.com>
2025-03-02 15:03:26 +0300

    feature/banner and isMobile and filter-types

---
+++ src/shared/hooks/useScreenWidth.ts
@@ -0,0 +1,28 @@
+import { useEffect } from 'react'
+
+import { setWidth } from 'app/model/screen.slice'
+
+import { useAppDispatch, useAppSelector } from 'shared/lib/hooks'
+
+export const useScreenWidth = (): { width: number; isMobile: boolean } => {
+  const dispatch = useAppDispatch()
+  const width = useAppSelector((state) => state.screen.width)
+
+  useEffect(() => {
+    const handleResize = () => {
+      dispatch(setWidth(window.innerWidth))
+    }
+
+    window.addEventListener('resize', handleResize)
+    handleResize()
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [dispatch])
+
+  return {
+    width,
+    isMobile: width <= 768,
+  }
+}
