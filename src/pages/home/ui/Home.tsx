5810bb1129d337ee070d39ec1dec26cc6eaf9449
bekeev-turat <101186144+bekeev-turat@users.noreply.github.com>
2025-03-08 18:28:57 +0600

    refactor: move popular products data to home page

--- src/pages/home/ui/Home.tsx
+++ src/pages/home/ui/Home.tsx
@@ -1,12 +1,19 @@
+import { popularProductsMock } from 'pages/home/api/data'
+
 import { MainBanner } from 'widgets/banner'
 import { HomeFilter } from 'widgets/home-filter/ui/HomeFilter'
 import { Partners } from 'widgets/partners'
-import { PopularProducts } from 'widgets/popular-products'
+import { ProductList } from 'widgets/product-list'
 import { Promotion } from 'widgets/promotion/ui/Promotion'
+
+import { NavigateBtn } from 'features/navigate-button'
 
 import { useGetHomeProductQuery } from '../api'
 
+import s from './Home.module.scss'
+
 export function Home() {
+  const products = popularProductsMock
   const { data } = useGetHomeProductQuery()
 
   console.log(data)
@@ -17,7 +24,11 @@ export function Home() {
 
       <HomeFilter />
 
-      <PopularProducts />
+      <section className={s.section}>
+        <h2 className={s.title}>Популярные шины</h2>
+        <ProductList products={products} />
+        <NavigateBtn>Посмотреть все шины</NavigateBtn>
+      </section>
 
       <Promotion />
 
