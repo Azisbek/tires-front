e906c1051ce3b57ada10c7f8e9b5516e65825aaa
bekeev-turat <bekeevturat06@gmail.com>
2025-03-11 10:03:05 +0600

    feat: add CatalogProducts widget with sorting

--- src/pages/catalog/ui/Catalog.tsx
+++ src/pages/catalog/ui/Catalog.tsx
@@ -1,10 +1,14 @@
+import { CatalogProducts } from 'widgets/catalog-products'
+
+import { InputSelect } from 'shared/ui/InputSelect/InputSelect'
 import { Text, Title } from 'shared/ui/Text'
 
 export function Catalog() {
   return (
     <div>
-      <Title size="sm-18">Hellow</Title>
+      <Title size="xl-40">Шины в Бишкеке</Title>
       <Text>Hello World</Text>
+      <CatalogProducts />
     </div>
   )
 }
