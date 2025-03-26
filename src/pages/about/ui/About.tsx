0a4a830094ddeb23abbcc023d487c63577794a19
erlan-tarielov-dev <erlan.tarielov.dev@gmail.com>
2025-03-26 20:48:24 +0600

    Edited Anchor.tsx component, updated size for img and container classes

--- src/pages/about/ui/About.tsx
+++ src/pages/about/ui/About.tsx
@@ -1,18 +1,24 @@
-import { AboutCompany } from "widgets/aboutCompany"
-import { PromotionList } from "widgets/promotionList/ui/PromotionList"
-import { AppButton } from "shared/ui/AppButton/AppButton"
-import s from './About.module.scss'
+import { AboutCompany } from 'widgets/aboutCompany'
+import { PromotionList } from 'widgets/promotionList/ui/PromotionList'
 
+import { AppButton } from 'shared/ui/AppButton/AppButton'
+
+import s from './About.module.scss'
 
 export const About = () => {
   return (
     <div className={s.container}>
-      <AboutCompany/>
+      <AboutCompany />
+
       <div className={s.promotionList}>
-         <PromotionList title="Новости и статьи"/>
-      <AppButton
-      className={s.btn}
-      variant="border">Посмотреть все новости и статьи</AppButton>
+        <PromotionList title="Новости и статьи" />
+        
+        <AppButton
+          className={s.btn}
+          variant="border"
+        >
+          Посмотреть все новости и статьи
+        </AppButton>
       </div>
     </div>
   )
