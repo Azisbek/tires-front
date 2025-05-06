ac543ac9bc6f0be26b289aa020fc27d78fa09b0a
akylbekova97 <ggdddrrr172@gmail.com>
2025-05-07 01:26:53 +0600

    fix/add-news

--- src/shared/model/navigation.ts
+++ src/shared/model/navigation.ts
@@ -17,6 +17,7 @@ export const navigationMap = {
   Admin: '/admin',
   AddProduct: 'add-product',
   ApplicationsList: 'applications',
+  AddNews: 'add-news',
 } as const
 
 export type NavigationMap = (typeof navigationMap)[keyof typeof navigationMap]
